659e812c786a965a12103cb3aaa2fc88dc2ddd71
Vladimir Seropol <frontender23@mail.ru>
2025-06-15 20:51:46 +0300

    edit loading1

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -7,10 +7,10 @@ export default function Loading() {
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-white/80">
       <div className="animate-spin-slow mb-4">
         <Image
-                  //   src="/car-loading.gif"
+                    src="/car-loading.gif"
                   alt="Loading"
                   width={100}
-                  height={100} src={""}        />
+                  height={100}       />
       </div>
       <p className="text-xl text-gray-500">Загрузка...</p>
     </div>
